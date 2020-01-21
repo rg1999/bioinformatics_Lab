5d3e6399ab1469a2870a83726c353f38bd641fcc
rg1999 <47385458+rg1999@users.noreply.github.com>
2020-01-21 15:58:00 +0530

    Add files via upload

---
+++ p8.pl
@@ -0,0 +1,11 @@
+#removed element from the begining
+
+@bases =('A','G','C','T');
+$base2 = shift @bases;
+print "Here is element removed from the begining";
+print $base2, "\n\n";
+print "here is the remaning array of the bases ";
+print "@bases\n";
+ 
+
+
