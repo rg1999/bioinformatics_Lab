4af8f28664f747bc5b1b7e0e5bee7be02ee8dd3c
rg1999 <47385458+rg1999@users.noreply.github.com>
2020-01-21 15:43:14 +0530

    Add files via upload

---
+++ p6.pl
@@ -0,0 +1,19 @@
+#!usr/bin/perl -w
+#if-elsif-else
+$word='rishi_raghu';
+if($word eq 'rishi_raghu')
+{
+	print("rishi_raghu\n");
+}
+elsif($word eq "raghu_rishi")
+{
+	print "raghu_rishi\n";
+}
+elsif($word eq "raghvendra")
+{
+	print "raghvnedra- the magic word";
+}
+else
+{
+	print "Is\"$word\"a peptide?this program is not sure.\n";
+}
